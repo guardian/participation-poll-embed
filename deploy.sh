778413768dc13e0662eabf45774d2acf771a0cc0
Gideon Goldberg <gidsgoldberg@gmail.com>
2016-06-08 11:09:04 +0100

    add cache control header

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ npm run eslint
 cp index.html bundle.js poll-static.json build/
 cp css/style.css build/css/style.css
 cp bower_components/pasteup-forms/build/forms.min.css build/bower_components/pasteup-forms/build/
-aws s3 sync build/ s3://gdn-cdn/participation/poll/embed --acl public-read --profile interactivesProd
+aws s3 sync build/ s3://gdn-cdn/participation/poll/embed --acl public-read --cache-control 'max-age=60' --profile interactivesProd
 rm -rf build/
 
